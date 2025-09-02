a4ed1e20a40daff42a4c1e21c68951b250d58c84
Mazureli <liammazure2001@gmail.com>
2025-09-02 15:41:23 -0400

    Fixed Hunts Views

--- Backend/Scavenger_Hunt_Db/users/models.py
+++ Backend/Scavenger_Hunt_Db/users/models.py
@@ -7,7 +7,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length= 30)
     email = models.CharField(max_length = 50, unique=True)
-    username = models.CharField(max_length = 30)
+    username = models.CharField(max_length = 30, unique=True)
     password = models.CharField(max_length=30)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
