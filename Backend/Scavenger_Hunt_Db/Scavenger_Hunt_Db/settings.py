a563b4afb8437516b392bcea518fffbee2d2069e
Mazureli <liammazure2001@gmail.com>
2025-09-03 17:21:47 -0400

    Added Logging

--- Backend/Scavenger_Hunt_Db/Scavenger_Hunt_Db/settings.py
+++ Backend/Scavenger_Hunt_Db/Scavenger_Hunt_Db/settings.py
@@ -15,6 +15,7 @@ from datetime import timedelta
 from dotenv import load_dotenv
 import os
 import dj_database_url
+import logging
 
 load_dotenv()
 
@@ -181,3 +182,17 @@ MEDIA_ROOT = BASE_DIR/"media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
+}
