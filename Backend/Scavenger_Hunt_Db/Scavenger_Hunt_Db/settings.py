7f711bc8c1fc4315e0b9573c0affe76fa08e8a69
Mazureli <liammazure2001@gmail.com>
2025-09-04 12:26:38 -0400

    Updated Settings

--- Backend/Scavenger_Hunt_Db/Scavenger_Hunt_Db/settings.py
+++ Backend/Scavenger_Hunt_Db/Scavenger_Hunt_Db/settings.py
@@ -33,6 +33,8 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', default='us-east-1')
+AWS_DEFAULT_ACL = 'public-read' #allow public read access
+AWS_QUERYSTRIN_AUTH = False #disable signed URLs
 
 # Use S3 for file storage
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
