ace00593ffaf0bf16853c7e5a190f19c2a434ad4
Mazureli <liammazure2001@gmail.com>
2025-09-08 13:21:51 -0400

    Updated Media URL

--- Backend/Scavenger_Hunt_Db/Scavenger_Hunt_Db/settings.py
+++ Backend/Scavenger_Hunt_Db/Scavenger_Hunt_Db/settings.py
@@ -184,7 +184,7 @@ STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 #Media-Files(user-upoaded)
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-MEDIA_ROOT = BASE_DIR/"media"
+# MEDIA_ROOT = BASE_DIR/"media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
